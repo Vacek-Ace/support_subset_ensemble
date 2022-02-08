b9ee8bff636f18f28194f628596bb1fe312962f1
Vacek-Ace <rodetlas@gmail.com>
2022-02-08 17:02:13 +0100

    datasets artificiales

--- src/data/toy_data.py
+++ src/data/toy_data.py
@@ -1,8 +1,22 @@
 import numpy as np
 import random 
+from scipy.stats import multivariate_normal
 
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+
+
+def normal_generator2(mu0, sigma0, n0, mu1, sigma1, n1, seed1, seed2):
+    mn0 = multivariate_normal(mean=mu0, cov=sigma0)
+    X_neg = mn0.rvs(size=n0, random_state=seed1)
+
+    mn1 = multivariate_normal(mean=mu1, cov=sigma1)
+    X_pos = mn1.rvs(size=n1, random_state=seed2)
+
+    X = np.vstack((X_neg, X_pos))
+    y = np.array([0] * len(X_neg) + [1] * len(X_pos))
+
+    return X, y
 
 
 def bubbles(mu1, mu2, sigma, n, target_class, seed):
